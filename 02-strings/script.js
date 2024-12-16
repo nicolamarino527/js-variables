c2e4560d2b767bc26699163834d4ea35d617451d
nicolamarino <nicolamarino527@gmail.com>
2024-12-16 17:09:38 +0100

    commit esercizi completi

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,23 +1,33 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
+const name = "Nicola";
+console.log("Nome:", name)
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+const number = 38
+const text = "é un numero comune"
+let bool = true;
+
+console.log("Number:", number);
+console.log("Text:", text); 
+console.log("Bool:", bool);
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
+
 const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
-console.log(fullName); // Output: "Jane Smith"
+const fullName = firstName + " " + lastName ;
+console.log("Nome completo:", fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = null;
-console.log(numberOfLettersInAlphabet); // Output: 26
+const numberOfLettersInAlphabet = alphabet.length;
+console.log("Numero di lettere nell alfabeto:", numberOfLettersInAlphabet); // Output: 26
